c012457896d6a10e10608ca430020c3fc9cd6e08
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2024-06-21 02:43:07 +0200

    show only functions and classes from import

--- fluxpy/__init__.py
+++ fluxpy/__init__.py
@@ -1,3 +1,13 @@
-from .utils import utils
+# Import functions
+from .utils import nonzero_fluxes, get_reactions_producing_met, get_nutrients_gradient, \
+    mapNamespaceWithModelSEEDDatabase, map2namespace, parse_qfca_output, samples_on_qfca, \
+    convert_gbk_to_faa  # format
+
+# Import classes
+from .utils import Models, NameIdConverter, CompareModels
+
 from .stats import extract_stats_from_solution
 from .illustrations import illustrations
+from .constants import SEED2MNX, BIGG2MNX
+
+__all__ = [name for name in dir() if not name.startswith('_')]
