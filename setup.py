c2c28d60d3f65a919af971678dd18fc65ab317ca
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2024-06-24 15:52:19 +0200

    add package_data

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='fluxpy',
     version='0.1',
     packages=find_packages(),
+    package_data={
+        'fluxpy': ['illustrations/*.json'],
+    },
     description='A set of tools to support metabolic modelling analysis related tasks.',
     author='Haris Zafeiropoulos',
     author_email='haris.zafeiropoulos@kuleuven.be',
